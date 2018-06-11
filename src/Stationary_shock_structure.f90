8726f017f87480a153dee8f641c914d3959e7c14
Yaphet-Chen <619112510@qq.com>
2018-06-12 00:01:22 +0800

    add more tools and modified Flux module

--- src/Stationary_shock_structure.f90
+++ src/Stationary_shock_structure.f90
@@ -43,9 +43,9 @@ module Mesh
 
     !Cell interface
     type CellInterface
-        !Flow field
-        real(KREAL)                                     :: leftVars(3) !Left conservative variables at cell interface
-        real(KREAL)                                     :: rightVars(3) !Right conservative variables at cell interface
+        ! !Flow field
+        ! real(KREAL)                                     :: left_h(3),left_b(3) !Left distribution function at cell interface
+        ! real(KREAL)                                     :: right_h(3),right_b(3) !Right distribution function at cell interface
         !Flux
         real(KREAL)                                     :: flux(3) !Conservative variables flux at cell interface
         real(KREAL), allocatable, dimension(:)          :: flux_h,flux_b !Flux of distribution function
@@ -69,15 +69,15 @@ module ControlParameters
     real(KREAL), parameter                              :: CFL = 0.95_KREAL !CFL number
     real(KREAL)                                         :: simTime = 0.0_KREAL !Current simulation time
     real(KREAL), parameter                              :: MAX_TIME = 250.0_KREAL !Maximal simulation time
-    integer(KINT), parameter                            :: MAX_ITER = 100 !Maximal iteration number
+    integer(KINT), parameter                            :: MAX_ITER = 1000 !Maximal iteration number
     integer(KINT)                                       :: iter = 1_KINT !Number of iteration
     real(KREAL)                                         :: dt !Global time step
 
     !Output control
     character(len=28), parameter                        :: HSTFILENAME = "StationaryShockStructure.hst" !History file name
     character(len=28), parameter                        :: RSTFILENAME = "StationaryShockStructure.dat" !Result file name
-    integer, parameter                                  :: HSTFILE = 20 !History file ID
-    integer, parameter                                  :: RSTFILE = 21 !Result file ID
+    integer(KINT), parameter                            :: HSTFILE = 20 !History file ID
+    integer(KINT), parameter                            :: RSTFILE = 21 !Result file ID
 
     !Gas propeties
     integer(KINT), parameter                            :: CK = 2 !Internal degree of freedom, here 2 denotes monatomic gas
@@ -87,7 +87,7 @@ module ControlParameters
     real(KREAL), parameter                              :: KN = 1.0 !Knudsen number in reference state
     real(KREAL), parameter                              :: ALPHA_REF = 1.0 !Coefficient in HS model
     real(KREAL), parameter                              :: OMEGA_REF = 0.5 !Coefficient in HS model
-    real(KREAL), parameter                              :: MU_REF !Viscosity coefficient in reference state
+    real(KREAL), parameter                              :: MU_REF = 5*(ALPHA_REF+1)*(ALPHA_REF+2)*sqrt(PI)/(4*ALPHA_REF*(5-2*OMEGA_REF)*(7-2*OMEGA_REF))*KN !Viscosity coefficient in reference state
     real(KREAL), parameter                              :: MA = 8.0 !Mach number
     !Geometry
     real(KREAL), parameter                              :: START_POINT = 0.0_KREAL, END_POINT = 50.0_KREAL
@@ -123,8 +123,8 @@ module ControlParameters
     !--------------------------------------------------
     integer(KINT)                                       :: uNum = 100 !Number of points in velocity space
     real(KREAL), parameter                              :: U_MIN = -15.0, U_MAX = 15.0 !Minimum and maximum micro velocity
-    real(KREAL), allocatable, dimension(:)              :: uSpace !discrete velocity space
-    real(KREAL), allocatable, dimension(:)              :: weight !weight at velocity u_k
+    real(KREAL), allocatable, dimension(:)              :: uSpace !Discrete velocity space
+    real(KREAL), allocatable, dimension(:)              :: weight !Qudrature weight at velocity u_k
 end module ControlParameters
 
 !--------------------------------------------------
@@ -149,6 +149,32 @@ contains
         GetConserved(2) = prim(1)*prim(2)
         GetConserved(3) = 0.5*prim(1)/(prim(3)*(GAMMA-1.0))+0.5*prim(1)*prim(2)**2
     end function GetConserved
+
+    !--------------------------------------------------
+    !>Convert conservative variables to primary variables
+    !>@param[in] w           :conservative variables
+    !>@return    GetPrimary  :primary variables
+    !--------------------------------------------------
+    function GetPrimary(w)
+        real(KREAL), intent(in)                         :: w(3) !Density, x-momentum, total energy
+        real(KREAL)                                     :: GetPrimary(3) !Density, x-velocity, lambda=1/temperature
+
+        GetPrimary(1) = w(1)
+        GetPrimary(2) = w(2)/w(1)
+        GetPrimary(3) = 0.5*w(1)/((GAMMA-1.0)*(w(3)-0.5*w(2)**2/w(1)))
+    end function GetPrimary
+
+    !--------------------------------------------------
+    !>Obtain speed of sound
+    !>@param[in] prim    :primary variables
+    !>@return    GetSoundSpeed :speed of sound
+    !--------------------------------------------------
+    function GetSoundSpeed(prim)
+        real(KREAL), intent(in)                         :: prim(3)
+        real(KREAL)                                     :: GetSoundSpeed !speed of sound
+
+        GetSoundSpeed = sqrt(0.5*GAMMA/prim(3))
+    end function GetSoundSpeed
 
     !--------------------------------------------------
     !>Obtain discretized Maxwellian distribution
@@ -193,6 +219,8 @@ end module Tools
 module Flux
     use Tools
     implicit none
+    integer(KREAL), parameter                           :: MNUM = 6 !Number of normal velocity moments
+
 contains
     !--------------------------------------------------
     !>Calculate flux of inner interface
@@ -201,42 +229,42 @@ contains
     !>@param[in]    rightCell :cell right to the target interface
     !--------------------------------------------------
     subroutine CalcFlux(leftCell,face,rightCell)
-        type(CellCenter), intent(in) :: leftCell,rightCell
-        type(CellInterface), intent(inout) :: face
-        real(KREAL), allocatable, dimension(:) :: h,b !distribution function at the interface
-        real(KREAL), allocatable, dimension(:) :: H0,B0 !Maxwellian distribution function
-        real(KREAL), allocatable, dimension(:) :: H_plus,B_plus !Shakhov part of the equilibrium distribution
-        real(KREAL), allocatable, dimension(:) :: sh,sb !slope of distribution function at the interface
-        integer(KINT), allocatable, dimension(:) :: delta !Heaviside step function
-        real(KREAL) :: w(3),prim(3) !conservative and primary variables at the interface
-        real(KREAL) :: qf !heat flux in normal and tangential direction
-        real(KREAL) :: sw(3) !slope of W
-        real(KREAL) :: aL(3),aR(3),aT(3) !micro slope of Maxwellian distribution, left,right and time.
-        real(KREAL) :: Mu(0:MNUM),Mu_L(0:MNUM),Mu_R(0:MNUM),Mxi(0:2) !<u^n>,<u^n>_{>0},<u^n>_{<0},<\xi^l>
-        real(KREAL) :: Mau_0(3),Mau_L(3),Mau_R(3),Mau_T(3) !<u\psi>,<aL*u^n*\psi>,<aR*u^n*\psi>,<A*u*\psi>
-        real(KREAL) :: tau !collision time
-        real(KREAL) :: Mt(5) !some time integration terms
-        integer :: i,j
+        type(CellCenter), intent(in)                    :: leftCell,rightCell
+        type(CellInterface), intent(inout)              :: face
+        real(KREAL), allocatable, dimension(:)          :: h,b !Distribution function at the interface
+        real(KREAL), allocatable, dimension(:)          :: H0,B0 !Maxwellian distribution function
+        real(KREAL), allocatable, dimension(:)          :: H_plus,B_plus !Shakhov part of the equilibrium distribution
+        real(KREAL), allocatable, dimension(:)          :: sh,sb !Slope of distribution function at the interface
+        integer(KINT), allocatable, dimension(:)        :: delta !Heaviside step function
+        real(KREAL)                                     :: conVars(3),prim(3) !Conservative and primary variables at the interface
+        real(KREAL)                                     :: qf !Heat flux in normal and tangential direction
+        real(KREAL)                                     :: sw(3) !Slope of conVars
+        real(KREAL)                                     :: aL(3),aR(3),aT(3) !Micro slope of Maxwellian distribution, left,right and time.
+        real(KREAL)                                     :: Mu(0:MNUM),MuL(0:MNUM),MuR(0:MNUM),Mxi(0:2) !<u^n>,<u^n>_{>0},<u^n>_{<0},<\xi^l>
+        real(KREAL)                                     :: Mau_0(3),Mau_L(3),Mau_R(3),Mau_T(3) !<u\psi>,<aL*u^n*\psi>,<aR*u^n*\psi>,<A*u*\psi>
+        real(KREAL)                                     :: tau !Collision time
+        real(KREAL)                                     :: Mt(5) !Some time integration terms
+        integer(KINT)                                   :: i,j
 
         !--------------------------------------------------
-        !prepare
+        !Prepare
         !--------------------------------------------------
-        !allocate array
-        allocate(delta(unum))
-        allocate(h(unum))
-        allocate(b(unum))
-        allocate(sh(unum))
-        allocate(sb(unum))
-        allocate(H0(unum))
-        allocate(B0(unum))
-        allocate(H_plus(unum))
-        allocate(B_plus(unum))
+        !Allocate array
+        allocate(delta(uNum))
+        allocate(h(uNum))
+        allocate(b(uNum))
+        allocate(sh(uNum))
+        allocate(sb(uNum))
+        allocate(H0(uNum))
+        allocate(B0(uNum))
+        allocate(H_plus(uNum))
+        allocate(B_plus(uNum))
 
         !Heaviside step function
         delta = (sign(UP,uspace)+1)/2
 
         !--------------------------------------------------
-        !reconstruct initial distribution
+        !Reconstruct initial distribution at interface
         !--------------------------------------------------
         h = (leftCell%h+0.5*leftCell%length*leftCell%sh)*delta+&
             (rightCell%h-0.5*rightCell%length*rightCell%sh)*(1-delta)
@@ -246,44 +274,41 @@ contains
         sb = leftCell%sb*delta+rightCell%sb*(1-delta)
 
         !--------------------------------------------------
-        !obtain macroscopic variables
+        !Obtain macroscopic variables
         !--------------------------------------------------
-        !conservative variables w_0
-        w(1) = sum(weight*h)
-        w(2) = sum(weight*uspace*h)
-        w(3) = 0.5*(sum(weight*uspace**2*h)+sum(weight*b))
+        !Conservative variables w_0 at interface
+        conVars(1) = sum(weight*h)
+        conVars(2) = sum(weight*uspace*h)
+        conVars(3) = 0.5*(sum(weight*uspace**2*h)+sum(weight*b))
 
-        !convert to primary variables
-        prim = get_primary(w)
-
-        !heat flux
-        qf = get_heat_flux(h,b,prim) 
+        !Convert to primary variables
+        prim = GetPrimary(conVars)
 
         !--------------------------------------------------
-        !calculate a^L,a^R
+        !Calculate a^L,a^R
         !--------------------------------------------------
-        sw = (w-leftCell%w)/(0.5*leftCell%length) !left slope of W
-        aL = micro_slope(prim,sw) !calculate a^L
+        sw = (conVars-leftCell%conVars)/(0.5*leftCell%length) !left slope of conVars
+        aL = MicroSlope(prim,sw) !calculate a^L
 
-        sw = (rightCell%w-w)/(0.5*rightCell%length) !right slope of W
-        aR = micro_slope(prim,sw) !calculate a^R
+        sw = (rightCell%conVars-conVars)/(0.5*rightCell%length) !right slope of conVars
+        aR = MicroSlope(prim,sw) !calculate a^R
 
         !--------------------------------------------------
-        !calculate time slope of W and A
+        !Calculate time slope of conVars and A
         !--------------------------------------------------
         !<u^n>,<\xi^l>,<u^n>_{>0},<u^n>_{<0}
-        call calc_moment_u(prim,Mu,Mxi,Mu_L,Mu_R) 
+        call CalcMoment(prim,Mu,Mxi,MuL,MuR) 
 
-        Mau_L = moment_au(aL,Mu_L,Mxi,1) !<aL*u*\psi>_{>0}
-        Mau_R = moment_au(aR,Mu_R,Mxi,1) !<aR*u*\psi>_{<0}
+        Mau_L = moment_au(aL,MuL,Mxi,1) !<aL*u*\psi>_{>0}
+        Mau_R = moment_au(aR,MuR,Mxi,1) !<aR*u*\psi>_{<0}
 
-        sw = -prim(1)*(Mau_L+Mau_R) !time slope of W
-        aT = micro_slope(prim,sw) !calculate A
+        sw = -prim(1)*(Mau_L+Mau_R) !time slope of conVars
+        aT = MicroSlope(prim,sw) !calculate A
 
         !--------------------------------------------------
         !calculate collision time and some time integration terms
         !--------------------------------------------------
-        tau = get_tau(prim)
+        tau = GetTau(prim)
 
         Mt(4) = tau*(1.0-exp(-dt/tau))
         Mt(5) = -tau*dt*exp(-dt/tau)+tau*Mt(4)
@@ -295,8 +320,8 @@ contains
         !calculate the flux of conservative variables related to g0
         !--------------------------------------------------
         Mau_0 = moment_uv(Mu,Mxi,1,0) !<u*\psi>
-        Mau_L = moment_au(aL,Mu_L,Mxi,2) !<aL*u^2*\psi>_{>0}
-        Mau_R = moment_au(aR,Mu_R,Mxi,2) !<aR*u^2*\psi>_{<0}
+        Mau_L = moment_au(aL,MuL,Mxi,2) !<aL*u^2*\psi>_{>0}
+        Mau_R = moment_au(aR,MuR,Mxi,2) !<aR*u^2*\psi>_{<0}
         Mau_T = moment_au(aT,Mu,Mxi,1) !<A*u*\psi>
 
         face%flux = Mt(1)*prim(1)*Mau_0+Mt(2)*prim(1)*(Mau_L+Mau_R)+Mt(3)*prim(1)*Mau_T
@@ -304,9 +329,12 @@ contains
         !--------------------------------------------------
         !calculate the flux of conservative variables related to g+ and f0
         !--------------------------------------------------
+        !Calculate heat flux
+        qf = GetHeatFlux(h,b,prim) 
+
         !Maxwellian distribution H0 and B0
         call discrete_maxwell(H0,B0,prim)
-
+    
         !Shakhov part H+ and B+
         call shakhov_part(H0,B0,qf,prim,H_plus,B_plus)
 
@@ -332,7 +360,94 @@ contains
                         Mt(2)*uspace**2*(aR(1)*B0+aR(2)*uspace*B0+0.5*aR(3)*(uspace**2*B0+Mxi(2)*H0))*(1-delta)+&
                         Mt(3)*uspace*(aT(1)*B0+aT(2)*uspace*B0+0.5*aT(3)*(uspace**2*B0+Mxi(2)*H0))+&
                         Mt(4)*uspace*b-Mt(5)*uspace**2*sb
+        
+        !--------------------------------------------------
+        !Aftermath
+        !--------------------------------------------------
+        !Deallocate array
+        deallocate(delta)
+        deallocate(h)
+        deallocate(b)
+        deallocate(sh)
+        deallocate(sb)
+        deallocate(H0)
+        deallocate(B0)
+        deallocate(H_plus)
+        deallocate(B_plus)
     end subroutine CalcFlux
+
+    !--------------------------------------------------
+    !>Calculate micro slope of Maxwellian distribution
+    !>@param[in] prim        :primary variables
+    !>@param[in] sw          :slope of W
+    !>@return    MicroSlope  :slope of Maxwellian distribution
+    !--------------------------------------------------
+    function MicroSlope(prim,sw)
+        real(KREAL), intent(in)                         :: prim(3),sw(3)
+        real(KREAL)                                     :: MicroSlope(3)
+
+        MicroSlope(3) = 4.0*prim(3)**2/((CK+1)*prim(1))*(2.0*sw(3)-2.0*prim(2)*sw(2)+sw(1)*(prim(2)**2-0.5*(CK+1)/prim(3)))
+        MicroSlope(2) = 2.0*prim(3)/prim(1)*(sw(2)-prim(2)*sw(1))-prim(2)*MicroSlope(3)
+        MicroSlope(1) = sw(1)/prim(1)-prim(2)*MicroSlope(2)-0.5*(prim(2)**2+0.5*(CK+1)/prim(3))*MicroSlope(3)
+    end function MicroSlope
+
+    !--------------------------------------------------
+    !>Calculate collision time
+    !>@param[in] prim    :primary variables
+    !>@return    GetTau  :collision time
+    !--------------------------------------------------
+    function GetTau(prim)
+        real(KREAL), intent(in)                         :: prim(3)
+        real(KREAL)                                     :: GetTau
+
+        GetTau = MU_REF*2.0*prim(3)**(1-OMEGA)/prim(1)
+    end function GetTau
+    
+    !--------------------------------------------------
+    !>Calculate heat flux
+    !>@param[in] h,b           :distribution function
+    !>@param[in] prim          :primary variables
+    !>@return    GetHeatFlux   :heat flux in normal and tangential direction
+    !--------------------------------------------------
+    function GetHeatFlux(h,b,prim)
+        real(KREAL), dimension(:), intent(in)           :: h,b
+        real(KREAL), intent(in)                         :: prim(3)
+        real(KREAL)                                     :: GetHeatFlux !heat flux in normal and tangential direction
+
+        GetHeatFlux = 0.5*(sum(weight*(uspace-prim(2))*(uspace-prim(2))**2*h)+sum(weight*(uspace-prim(2))*b)) 
+    end function GetHeatFlux
+
+    !--------------------------------------------------
+    !>calculate moments of velocity and \xi
+    !>@param[in] prim :primary variables
+    !>@param[out] Mu        :<u^n>
+    !>@param[out] Mxi       :<\xi^2n>
+    !>@param[out] MuL,MuR   :<u^n>_{>0},<u^n>_{<0}
+    !--------------------------------------------------
+    subroutine CalcMoment(prim,Mu,Mxi,MuL,MuR)
+        real(KREAL), intent(in)                         :: prim(3)
+        real(KREAL), intent(out)                        :: Mu(0:MNUM),MuL(0:MNUM),MuR(0:MNUM)
+        real(KREAL), intent(out)                        :: Mxi(0:2)
+        integer :: i
+
+        !Moments of normal velocity
+        MuL(0) = 0.5*erfc(-sqrt(prim(3))*prim(2))
+        MuL(1) = prim(2)*MuL(0)+0.5*exp(-prim(3)*prim(2)**2)/sqrt(PI*prim(3))
+        MuR(0) = 0.5*erfc(sqrt(prim(3))*prim(2))
+        MuR(1) = prim(2)*MuR(0)-0.5*exp(-prim(3)*prim(2)**2)/sqrt(PI*prim(3))
+
+        do i=2,MNUM
+            MuL(i) = prim(2)*MuL(i-1)+0.5*(i-1)*MuL(i-2)/prim(3)
+            MuR(i) = prim(2)*MuR(i-1)+0.5*(i-1)*MuR(i-2)/prim(3)
+        end do
+
+        Mu = MuL+MuR
+
+        !Moments of \xi
+        Mxi(0) = 1.0 !<\xi^0>
+        Mxi(1) = 0.5*CK/prim(3) !<\xi^2>
+        Mxi(2) = CK*(CK+2.0)/(4.0*prim(3)**2) !<\xi^4>
+    end subroutine CalcMoment
 end module Flux
 !--------------------------------------------------
 !>UGKS solver
@@ -373,6 +488,11 @@ contains
 
     !--------------------------------------------------
     !>Reconstruct the slope of initial distribution function
+    !>Index method
+    ! -------------------------------
+    ! | (i-1) |  (i) |  (i) | (i+1) |
+    ! | cell  | face | cell | face  |
+    ! -------------------------------
     !--------------------------------------------------
     subroutine Reconstruction()
         integer(KINT) :: i
